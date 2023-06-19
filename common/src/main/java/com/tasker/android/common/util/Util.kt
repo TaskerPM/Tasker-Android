e29e22af74ed4f2baae1302ba43a9268f7cce9df
dongk00 <dddongk00@gmail.com>
2023-06-19 11:07:30 +0900

    [chore] HomeTaskDesignHelper

--- common/src/main/java/com/tasker/android/common/util/Util.kt
+++ common/src/main/java/com/tasker/android/common/util/Util.kt
@@ -1,9 +1,11 @@
 package com.tasker.android.common.util
 
 import android.content.Context
+import android.graphics.drawable.Drawable
 import androidx.annotation.ColorRes
 import androidx.annotation.DrawableRes
 import androidx.core.content.res.ResourcesCompat
+import androidx.core.graphics.drawable.DrawableCompat
 import java.time.DayOfWeek
 import java.time.LocalDate
 import java.time.format.TextStyle
@@ -14,6 +16,11 @@ fun getDrawableFrom(context: Context, @DrawableRes Id: Int) =
 
 fun getColorFrom(context: Context, @ColorRes Id: Int) =
     context.resources.getColor(Id, null)
+
+fun applyDrawableTintColor(context: Context, drawable: Drawable, @ColorRes Id: Int) {
+    val wrappedDrawable = DrawableCompat.wrap(drawable)
+    DrawableCompat.setTint(wrappedDrawable, getColorFrom(context, Id))
+}
 
 fun getDayOfWeekValue(year: Int, month: Int, day: Int): Int {
     return LocalDate.of(year, month, day).dayOfWeek.value
--- feature/home/src/main/java/com/tasker/android/home/presentation/list/HomeListViewAdapter.kt
+++ feature/home/src/main/java/com/tasker/android/home/presentation/list/HomeListViewAdapter.kt
@@ -2,18 +2,13 @@ package com.tasker.android.home.presentation.list
 
 import android.content.Context
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.ContextCompat
-import androidx.core.graphics.drawable.DrawableCompat
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.tasker.android.common.util.getColorFrom
-import com.tasker.android.common.util.getDrawableFrom
-import com.tasker.android.home.R
 import com.tasker.android.home.databinding.ItemHomeListViewBinding
 import com.tasker.android.home.presentation.model.HomeTaskData
+import com.tasker.android.home.presentation.util.HomeTaskDesignHelper
 
 class HomeListViewAdapter :
     ListAdapter<HomeTaskData, HomeListViewAdapter.HomeListViewHolder>(HomeListViewDiffUtil()) {
@@ -41,95 +36,8 @@ class HomeListViewAdapter :
             binding.apply {
                 tvHomeTaskText.text = data.text
 
-                when (data.isCompleted) {
-                    true -> {
-                        ivHomeTaskIsCompleted.setImageResource(R.drawable.icon_home_task_completed)
-                        vHomeTaskIsCompleted.visibility = View.VISIBLE
-                        tvHomeTaskText.setTextColor(
-                            getColorFrom(context, com.tasker.android.common.R.color.gray_230)
-                        )
-
-                        if (data.categoryTag.isNotEmpty()) {
-                            tvHomeTaskTagCategory.text = data.categoryTag
-                            DrawableCompat.setTint(
-                                DrawableCompat.wrap(tvHomeTaskTagCategory.background),
-                                ContextCompat.getColor(
-                                    context,
-                                    com.tasker.android.common.R.color.gray_30
-                                )
-                            )
-                            tvHomeTaskTagCategory.setTextColor(
-                                getColorFrom(context, com.tasker.android.common.R.color.gray_230)
-                            )
-                        }
-
-                        if (data.timeTag.isNotEmpty()) {
-                            tvHomeTaskTagTime.text = data.timeTag
-                            DrawableCompat.setTint(
-                                DrawableCompat.wrap(tvHomeTaskTagTime.background),
-                                ContextCompat.getColor(
-                                    context,
-                                    com.tasker.android.common.R.color.gray_30
-                                )
-                            )
-                            tvHomeTaskTagTime.setTextColor(
-                                getColorFrom(context, com.tasker.android.common.R.color.gray_230)
-                            )
-                        }
-                    }
-                    false -> {
-                        ivHomeTaskIsCompleted.setImageResource(R.drawable.icon_home_task_uncompleted)
-                        vHomeTaskIsCompleted.visibility = View.GONE
-                        tvHomeTaskText.setTextColor(
-                            getColorFrom(context, com.tasker.android.common.R.color.black_basic)
-                        )
-
-                        if (data.categoryTag.isNotEmpty()) {
-                            tvHomeTaskTagCategory.text = data.categoryTag
-                            DrawableCompat.setTint(
-                                DrawableCompat.wrap(tvHomeTaskTagCategory.background),
-                                ContextCompat.getColor(
-                                    context,
-                                    com.tasker.android.common.R.color.temp_tag_category
-                                )
-                            )
-                            tvHomeTaskTagCategory.setTextColor(
-                                getColorFrom(
-                                    context,
-                                    com.tasker.android.common.R.color.temp_tag_text_category
-                                )
-                            )
-                        }
-
-                        if (data.timeTag.isNotEmpty()) {
-                            tvHomeTaskTagTime.text = data.timeTag
-                            DrawableCompat.setTint(
-                                DrawableCompat.wrap(tvHomeTaskTagTime.background),
-                                ContextCompat.getColor(
-                                    context,
-                                    com.tasker.android.common.R.color.temp_tag_time
-                                )
-                            )
-                            tvHomeTaskTagTime.setTextColor(
-                                getColorFrom(
-                                    context,
-                                    com.tasker.android.common.R.color.temp_tag_text_time
-                                )
-                            )
-                        }
-                    }
-                }
-
-                when (data.isFilled) {
-                    true -> {
-                        clHomeTask.background =
-                            getDrawableFrom(context, R.drawable.background_item_home_task_filled)
-                    }
-                    false -> {
-                        clHomeTask.background =
-                            getDrawableFrom(context, R.drawable.background_item_home_task_unfilled)
-                    }
-                }
+                val homeTaskDesignHelper = HomeTaskDesignHelper(context, binding = binding)
+                homeTaskDesignHelper.applyDesign(data = data)
             }
         }
     }
